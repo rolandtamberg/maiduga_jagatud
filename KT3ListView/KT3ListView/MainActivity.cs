adfb3f721e32a8ed07b1790bbc57b5e672ea16b9
Roland Tamberg <roland.tamberg@tptlive.ee>
2019-12-17 08:52:52 +0200

    Kodutöö3 test

---
+++ KT3ListView/KT3ListView/MainActivity.cs
@@ -0,0 +1,82 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Refit;
+using EDMTDialog;
+using System.Collections.Generic;
+using System;
+
+namespace KT3ListView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        
+        MyAPI myAPI;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button button_search = FindViewById<Button>(Resource.Id.btn_search);
+            ListView list_teams = FindViewById<ListView>(Resource.Id.lst_teams);
+            EditText enter_team = FindViewById<EditText>(Resource.Id.entr_team);
+
+            //Init API
+            myAPI = RestService.For<MyAPI>("https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=");
+
+            button_search.Click += async delegate
+            {
+                try
+                {
+                    Android.Support.V7.App.AlertDialog dialog = new EDMTDialogBuilder()
+                   .SetContext(this)
+                   .SetMessage("Please wait...")
+                   .Build();
+
+                    if (!dialog.IsShowing)
+                        dialog.Show();
+
+                    List<Team> teams = await myAPI.GetTeams();
+                    List<string> team_name = new List<string>();
+
+                    foreach (var team in teams)
+                        team_name.Add(team.strTeam);
+
+                    var adapter = new ArrayAdapter<string>(this,
+                        Android.Resource.Layout.SimpleListItem1, team_name);
+
+                    list_teams.Adapter = adapter;
+
+                    if (dialog.IsShowing)
+                        dialog.Dismiss();
+                }
+                catch (Exception ex)
+                {
+                    Toast.MakeText(this, "" + ex.Message, ToastLength.Long).Show();
+                }
+            };
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ KT3ListView/KT3ListView/Team.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KT3ListView
+{
+    public class Team
+    {
+        public string idTeam { get; set; }
+        public string idSoccerXML { get; set; }
+        public string intLoved { get; set; }
+        public string strTeam { get; set; }
+        public string strTeamShort { get; set; }
+        public string strAlternate { get; set; }
+        public string intFormedYear { get; set; }
+        public string strSport { get; set; }
+        public string strLeague { get; set; }
+        public string idLeague { get; set; }
+        public object strDivision { get; set; }
+        public string strManager { get; set; }
+        public string strStadium { get; set; }
+        public string strKeywords { get; set; }
+        public string strRSS { get; set; }
+        public string strStadiumThumb { get; set; }
+        public string strStadiumDescription { get; set; }
+        public string strStadiumLocation { get; set; }
+        public string intStadiumCapacity { get; set; }
+        public string strWebsite { get; set; }
+        public string strFacebook { get; set; }
+        public string strTwitter { get; set; }
+        public string strInstagram { get; set; }
+        public string strDescriptionEN { get; set; }
+        public string strDescriptionDE { get; set; }
+        public object strDescriptionFR { get; set; }
+        public object strDescriptionCN { get; set; }
+        public string strDescriptionIT { get; set; }
+        public object strDescriptionJP { get; set; }
+        public object strDescriptionRU { get; set; }
+        public string strDescriptionES { get; set; }
+        public object strDescriptionPT { get; set; }
+        public object strDescriptionSE { get; set; }
+        public object strDescriptionNL { get; set; }
+        public object strDescriptionHU { get; set; }
+        public object strDescriptionNO { get; set; }
+        public object strDescriptionIL { get; set; }
+        public object strDescriptionPL { get; set; }
+        public string strGender { get; set; }
+        public string strCountry { get; set; }
+        public string strTeamBadge { get; set; }
+        public string strTeamJersey { get; set; }
+        public string strTeamLogo { get; set; }
+        public string strTeamFanart1 { get; set; }
+        public string strTeamFanart2 { get; set; }
+        public string strTeamFanart3 { get; set; }
+        public string strTeamFanart4 { get; set; }
+        public string strTeamBanner { get; set; }
+        public string strYoutube { get; set; }
+        public string strLocked { get; set; }
+    }
+}
